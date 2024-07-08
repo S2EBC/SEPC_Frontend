6db0e757d6a4acd605e669841041a96691abbd67
huise0ng <shinhuiseong07@gmail.com>
2024-07-08 11:14:39 +0900

    ADD :: 시작 페이지

---
+++ hk/src/components/scanf.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/scanf.css'; // 올바른 경로로 수정
+
+const Scanf = () => {
+    return (
+        <div className="container">
+            <h1>BSSM 키오스크에 오신 것을 환영합니다.</h1>
+            <button>시작하기</button>
+        </div>
+    );
+}
+
+export default Scanf;
